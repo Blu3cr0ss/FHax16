71752a78fc5e57f97f5e42b2fb5a8b0b65f11cd5
Blu3cr0ss <elantsew.andrew@gmail.com>
2022-12-12 11:48:51 +0300

    minor changes

--- src/main/java/idk/bluecross/fhax16/FHax16.kt
+++ src/main/java/idk/bluecross/fhax16/FHax16.kt
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import org.reflections.Reflections
+import org.reflections.scanners.SubTypesScanner
 
 
 @Mod("fhax16")
@@ -28,26 +29,31 @@ class FHax16 {
     private fun doClientStuff(event: FMLClientSetupEvent) {
         LOGGER.info("doClientStuff()")
         // init all modules by reflection
-        val allClasses = Reflections("idk.bluecross.fhax16.module").getSubTypesOf(Module::class.java)
-        allClasses.filter {
-            !it.kotlin.isAbstract
-        }
-        LOGGER.info(allClasses)
-        allClasses.forEach {
-            (it.classLoader.loadClass(it.canonicalName).kotlin.objectInstance as? Module)?.let { module ->
-                val pkg = it.`package`.name.split(".").last()
-                module.category = module.getCategoryByString(pkg)
-            }
-        }
+        modulesLoader()
         //
+        LOGGER.info(modules)
         KeyListener //init
         ChatCommandsManager
         ConfigManager
         try {
             ConfigManager.getCfg()
             ConfigManager.getBinds()
-        }catch (e:Exception){
+        } catch (e: Exception) {
             LOGGER.error("Failed loading cfg on startup")
+        }
+    }
+
+    fun modulesLoader() {
+        LOGGER.warn("Just ignore reflections errors")
+        val refl = Reflections("idk.bluecross.fhax16.module", SubTypesScanner())
+        val allClasses = refl.getSubTypesOf(Module::class.java)
+        allClasses.filter {
+            !it.kotlin.isAbstract
+        }.forEach {
+            (it.classLoader.loadClass(it.canonicalName).kotlin.objectInstance as? Module)?.let { module ->
+                val pkg = it.`package`.name.split(".").last()
+                module.category = module.getCategoryByString(pkg)
+            }
         }
     }
 }
